23f9eb77e6a78b929f5cfeaf8c540d6670aa1eb9
jujinyoung <jujinyoung1838@naver.com>
2022-09-05 22:14:56 +0900

    5.2강 구글 로그인연동완료

--- src/main/java/com/example/springboot/web/web/IndexController.java
+++ src/main/java/com/example/springboot/web/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.springboot.web.web;
 
+import com.example.springboot.web.config.auth.LoginUser;
+import com.example.springboot.web.config.auth.dto.SessionUser;
 import com.example.springboot.web.domain.posts.PostsService;
 import com.example.springboot.web.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
